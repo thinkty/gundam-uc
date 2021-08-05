d8f98b754b68ce19481646d2197d06c920a184c9
thinkty <thinkty@naver.com>
2021-08-05 20:41:24 +0900

    Added header and tooltip

--- src/EventCardCastSection.js
+++ src/EventCardCastSection.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const style = {
   container: {
 
+  },
+  header: {
+    color: '#ffffff',
+    fontFamily: 'sans-serif',
+    fontSize: 18,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    margin: 10,
   },
   chipContainer: {
     display: 'flex',
@@ -13,6 +21,10 @@ const style = {
   },
   chip: {
     flexShrink: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     margin: 5,
     padding: 10,
     borderWidth: 1,
@@ -24,19 +36,41 @@ const style = {
     color: '#ffffff',
     fontSize: 16,
   },
-  chipImage: {
-
+  chipTooltip: {
+    width: 200,
+    backgroundColor: '#aaaaaa',
+    color: '#ffffff',
+    fontFamily: 'sans-serif',
+    fontSize: 16,
+    borderRadius: 5,
+    padding: 5,
+    zIndex: 10,
+    position: 'absolute',
+    top: 50,
+    left: 10,
   },
 };
 
 function CharacterChip({ character }) {
-  const { name, link, img } = character;
+  const { name, desc, link } = character;
+  const [isHovering, setHover] = useState(false);
+
   return (
-    <div
-      style={style.chip}
-      onClick={() => { window.open(link); }}
-    >
-      { name }
+    <div style={{ position: 'relative' }}>
+      <div
+        style={style.chip}
+        onClick={() => { window.open(link); }}
+        onMouseEnter={() => { setHover(true); }}
+        onMouseLeave={() => { setHover(false); }}
+      >
+        { name }
+      </div>
+      {
+        isHovering &&
+        <span style={style.chipTooltip}>
+          { desc }
+        </span>
+      }
     </div>
   );
 }
@@ -44,6 +78,9 @@ function CharacterChip({ character }) {
 export default function EventCardCastSection({ isMobile, characters, mobiles }) {
   return (
     <div style={style.container}>
+      <div style={style.header}>
+        { '[ Characters ]' }
+      </div>
       <div style={style.chipContainer}>
         {
           characters.map((character) => (
